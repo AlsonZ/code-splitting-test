9c2ea24c354a0adf454e4802efcafc06efb848d3
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-02-09 07:56:51 +1100

    Updates

---
+++ precache-manifest.636464386c2bc67dfe2a22eb8da0cfaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619b745f3dad538741b60c79b74fdd53",
+    "url": "/code-splitting-test/index.html"
+  },
+  {
+    "revision": "b2c98ac2ed2eb8bca761",
+    "url": "/code-splitting-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3cc7794128e8f5772e84",
+    "url": "/code-splitting-test/static/js/2.e8e9c247.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/code-splitting-test/static/js/2.e8e9c247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da51233ee0730cbe8046",
+    "url": "/code-splitting-test/static/js/3.e136b970.chunk.js"
+  },
+  {
+    "revision": "c378089fa29836ff6f70",
+    "url": "/code-splitting-test/static/js/4.77faebd5.chunk.js"
+  },
+  {
+    "revision": "b2c98ac2ed2eb8bca761",
+    "url": "/code-splitting-test/static/js/main.f322885f.chunk.js"
+  },
+  {
+    "revision": "8bbe0c9e5f00832a308a",
+    "url": "/code-splitting-test/static/js/runtime-main.144c8b3f.js"
+  }
+]);
---
+++ static/js/3.e136b970.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcode-splitting-test"]=this["webpackJsonpcode-splitting-test"]||[]).push([[3],{30:function(t,e,s){"use strict";s.r(e);var n=s(0),a=s.n(n);s(17);e.default=function(){return a.a.createElement("div",{className:"about"},"About page")}}}]);
+//# sourceMappingURL=3.e136b970.chunk.js.map
